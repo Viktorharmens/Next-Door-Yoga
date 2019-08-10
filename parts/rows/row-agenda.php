7d11db292f954b5939afb533c36c8313c1b6ae8b
LAPTOP-DELLXPS\Viktor <info@studiovuurkever.nl>
2019-08-10 12:38:48 +0200

    accepted changes after updating fil on gitHub

--- parts/rows/row-agenda.php
+++ parts/rows/row-agenda.php
@@ -27,11 +27,8 @@
                                 <li class="agenda__column">' . $row['day'] . '</li>
                                 <li class="agenda__column">' . $row['start_time'] . '-' . $row['end_time'] . '</li>
                                 <li class="agenda__column"><a href="' . $row['type']['url'] . '">' . $row['type']['title'] . '</a></li>
-<<<<<<< HEAD
                                 <li class="agenda__column teacher"><a href="' . $row['teacher']['url'] . '">' . $row['teacher']['title'] . '</a></li>
-=======
                                 <li class="agenda__column"><a href="' . $row['teacher']['url'] . '">' . $row['teacher']['title'] . '</a></li>
->>>>>>> 4a068d703b5f39f522e02648659dab49f4ef23f8
                                 <li class="agenda__column">' . $row['location'] . '</li>
                             </ul>';
                         
@@ -40,8 +37,7 @@
                                 <li class="agenda__column">' . $row['day'] . '</li>
                                 <li class="agenda__column">' . $row['start_time'] . '-' . $row['end_time'] . '</li>
                                 <li class="agenda__column"><a href="' . $row['type']['url'] . '">' . $row['type']['title'] . '</a></li>
-<<<<<<< HEAD
-                                <li class="agenda__column teacher"><a href="' . $row['teacher']['url'] . '">' . $row['teacher']['title'] . '</a></li>
+                                <li class="agenda__column"><a href="' . $row['teacher']['url'] . '">' . $row['teacher']['title'] . '</a></li>
                                 <li class="agenda__column">' . $row['location'] . '</li>
                             </ul>';
 
@@ -52,9 +48,6 @@
                                 <li class="agenda__column">' . $row['start_time'] . '-' . $row['end_time'] . '</li>
                                 <li class="agenda__column stijl"><a href="' . $row['type']['url'] . '">' . $row['type']['title'] . '</a></li>
                                 <li class="agenda__column teacher"><a href="' . $row['teacher']['url'] . '">' . $row['teacher']['title'] . '</a></li>
-=======
-                                <li class="agenda__column"><a href="' . $row['teacher']['url'] . '">' . $row['teacher']['title'] . '</a></li>
->>>>>>> 4a068d703b5f39f522e02648659dab49f4ef23f8
                                 <li class="agenda__column">' . $row['location'] . '</li>
                             </ul>';
                         }
